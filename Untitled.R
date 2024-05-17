ef6aae877a259c0ac64df4e1ef53cffa3acbcc24
Diyor Pardaev <80511099+diyorpardaev@users.noreply.github.com>
2024-05-17 15:28:59 +0900

    Hello World added

---
+++ Untitled.R
@@ -0,0 +1,8 @@
+print('hello world')
+2+3
+(3+6)*8
+2^3
+print(2+3) #2 va 3ni yig'indisini konsolga chiqaramiz
+log(10)+5.        #logarifmik funksiya
+sqrt(25).         #kvadrat ildiz
+max(5,3,2).       #eng katta qiymat
